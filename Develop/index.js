955b6947da80417779da353837e58b9ca0b3f355
Derrick <derrickedowen@hotmail.com>
2022-11-03 12:13:59 -0400

    Adding finished README generator code.

--- Develop/index.js
+++ Develop/index.js
@@ -1,21 +1,161 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 
-inquirer
-    .prompt([
-{}
 
-])
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+        validate: titleInput => {
+if (titleInput) {
+    return true;
+}else {
+    console.log('Please enter your project title.');
+    return false;
+}
+}
+},
+{
+    type: 'input',
+    message: 'Please provide a description of your project.',
+    name: 'description', 
+    validate: descriptionInput => {
+        if (descriptionInput) {
+            return true;
+        }else {
+            console.log('Please enter a project description.');
+            return false;
+        }
+}
+},
+{
+type: 'input',
+message: 'Please provide installation instructions for your project.',
+name: 'instructions',
+validate: instructionsInput => {
+    if (instructionsInput) {
+        return true;
+    }else {
+        console.log('Please enter installation instructions.');
+        return false;
+    }
+}
+},
+{
+type: 'input',
+message: 'Please provide usage information for your project.',
+name: 'usage',
+validate: usageInput => {
+    if (usageInput) {
+        return true;
+    }else {
+        console.log('Please enter usage information.');
+        return false;
+    }
+}
+},
+{
+type: 'input',
+message: 'Please provide contribution guidelines for your project.',
+name: 'contributions',
+validate: contributionsInput => {
+    if (contributionsInput) {
+        return true;
+    }else {
+        console.log('Please enter contribution guidelines if any.');
+        return false;
+    }
+}
+},
+{
+type: 'input',
+message: 'Please provide testing instructions for your project.',
+name: 'test',
+validate: testInput => {
+    if (testInput) {
+        return true;
+    }else {
+        console.log('Please enter testing instructions if any.');
+        return false;
+    }
+}
+},
+{
+type: 'checkbox',
+message: 'Please select a license for your project',
+name: 'license',
+choices: ['apache','agpl-3.0','gpl-3.0',
+'lgpl-3.0','MIT', 'unlicense','mpl-2.0','bsl-1.0', 'No License'],
+validate: licenseInput => {
+    if (licenseInput) {
+        return true;
+    }else {
+        console.log('Please select a license');
+        return false;
+    }
+}
+},
+{
+type: 'input',
+message: 'What is your Github username?',
+name: 'username',   
+validate: usernameInput => {
+    if (usernameInput) {
+        return true;
+    }else {
+        console.log('Please enter a username');
+        return false;
+    }
+}
+},
+{
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',   
+    validate: emailInput => {
+        if (emailInput) {
+            return true;
+        }else {
+            console.log('Please enter your email address');
+            return false;
+        }
+    }  
+},
+{
+    type: 'input',
+    message: 'Please provide instructions on how to contact you',
+    name: 'question',   
+    validate: questionInput => {
+        if (questionInput) {
+            return true;
+        }else {
+            return false;
+        }
+    }  
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log('Apparent error'+ err);
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile('GeneratedREADME.md', generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+if (license !== 'No License') {
+  return `![badge](https://img.shields.io/badge/License-${license}-red)`;
+} else {
+  return '';
+}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'No License') {
+    return `![${license}](https://choosealicense.com/licenses/${license})`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !=='No License'){
+    return `*[License](#License)`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
-}
+  ## Description
+  ${renderLicenseBadge(data.license)}
+  ${data.description}
+
+  ## Table of Contents
+  *[Description](#Description)
+  *[Installation](#Installation)
+  *[Usage](#Usage)
+  *[Contribution](#Contribution)
+  *[Testing](#Testing)
+  *[Questions](#Questions)
+  ${renderLicenseSection(data.license)}
+
+
+  ## Installation
+  ${data.instructions}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+The License Applicable to this application:
+ - ${renderLicenseLink(data.license)}
+
+  ## Contribution
+  ${data.contributions}
+
+  ## Testing
+  ${data.test}
+
+  ## Questions
+  Github: [${data.username}](https://github.com/${data.username})
+  Email: ${data.email}
+
+`;}
+
 
 module.exports = generateMarkdown;
